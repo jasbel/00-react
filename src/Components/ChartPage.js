d05fba5c1e436bd1e3974fd5c37ffe20f5a5110e
Asbel Apaza <asbel.elmec@gmail.com>
2020-12-05 14:23:54 -0400

    ADD code

--- src/Components/ChartPage.js
+++ src/Components/ChartPage.js
@@ -21,7 +21,20 @@ class ChartPage extends Component {
         },
       ],
     };
+
+    const newSeries = [];
   }
+
+  this.state.series.map((s) => {
+    const data = s.data.map(() => {
+      return Math.floor(Math.random() * (180 - min + 1)) + min
+    })
+    newSeries.push({ data, name: s.name })
+  })
+
+  this.setState({
+    series: newSeries
+  })
 
   render() {
     return (
